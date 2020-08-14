ecc9e919507be9462795d90acd9c270fe73b772b
WLeondike <billyleondike@gmail.com>
2020-08-13 22:43:40 -0400

    Updates

---
+++ precache-manifest.31816ee16ccd1c143b2558fa75d1bbf7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c7983bf420363e578475a3e0fce726",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "f2d9a97e06b128718d7a",
+    "url": "/reactPortfolio/static/css/main.60e56736.chunk.css"
+  },
+  {
+    "revision": "b5a47861aea26606f045",
+    "url": "/reactPortfolio/static/js/2.4933faa9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactPortfolio/static/js/2.4933faa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d9a97e06b128718d7a",
+    "url": "/reactPortfolio/static/js/main.1d106319.chunk.js"
+  },
+  {
+    "revision": "2f4d685e618f546a2f8a",
+    "url": "/reactPortfolio/static/js/runtime-main.506f4783.js"
+  },
+  {
+    "revision": "c21bb2063938cd4f1fd551920e58efc4",
+    "url": "/reactPortfolio/static/media/Full-Stack-Resume.c21bb206.pdf"
+  },
+  {
+    "revision": "d1b16d56f70fa93dae84744587e39e25",
+    "url": "/reactPortfolio/static/media/beach.d1b16d56.jpg"
+  },
+  {
+    "revision": "71efedfa5edf2f3e6557b0558ea4d066",
+    "url": "/reactPortfolio/static/media/burgerLogger.71efedfa.png"
+  },
+  {
+    "revision": "24fc7c86ba29161fe88295d257782c73",
+    "url": "/reactPortfolio/static/media/dayPlanner.24fc7c86.png"
+  },
+  {
+    "revision": "27c1f7afb5d37138c7a893cbfec53659",
+    "url": "/reactPortfolio/static/media/gitGudTogether.27c1f7af.png"
+  },
+  {
+    "revision": "c3b2c8119ddad539d757bc546542896a",
+    "url": "/reactPortfolio/static/media/headshot.c3b2c811.png"
+  },
+  {
+    "revision": "7016c96e61f205caa86ef6b127f0dc9d",
+    "url": "/reactPortfolio/static/media/streamJunkie.7016c96e.png"
+  }
+]);
